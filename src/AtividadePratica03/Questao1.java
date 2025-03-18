73da32b3b0e87bd27eb048ccc0f155884c33f587
DayeneS <dayene-santos@hotmail.com>
2025-03-17 23:39:33 -0300

    Questão 01 - Solicitar número inteiro e exibir valor

---
+++ src/AtividadePratica03/Questao1.java
@@ -0,0 +1,21 @@
+package AtividadePratica03;
+
+import java.util.Scanner;
+
+public class Questao1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.println("Digite um número: ");
+			num = sc.nextInt();
+			
+		System.out.println("O número digitado foi: " + num);
+		
+			
+		sc.close();
+	}
+
+}
